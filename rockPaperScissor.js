7244c9a14868137ef0c2b810d626993048f003a2
grufre349 <grufre349@gmail.com>
2025-07-26 13:50:08 +0200

    working version of rock, paper, scissors

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,21 +1,19 @@
 // processing input arguments
 const args = process.argv.slice(2);
-// print processed arguments
-// console.log(args);
 
 // check how many arguments are given
 if (args.length !== 1) {
 	console.error("Please provide exactly one argument");
+	return;
 }
-//convert input to lowercase for easier comparison
+// try to convert input to lowercase for easier comparison
 try {
 	lowerCaseInput = args[0].toLowerCase();
-	// console.log(input);
 } catch (error) {
-	console.error("Wrong input, try again with a string");
+	console.error("Wrong input, try again with a string?");
 }
 
-// Function to generate random integers
+// Function to generate random integers because its not inbuilt, thx to Gemini
 function getRandomInt(min, max) {
 	// ceil both values
 	min = Math.ceil(min);
@@ -25,9 +23,8 @@ function getRandomInt(min, max) {
 }
 // create a random number between 1 and 3 for enemys choice
 const randomNumber = getRandomInt(1, 3);
-console.log("generated num: " + randomNumber);
-console.log(typeof randomNumber);
 
+//switch case statement to "translate" random number to string
 let enemyChoice = "";
 switch (randomNumber) {
 	case 1:
@@ -40,4 +37,38 @@ switch (randomNumber) {
 		enemyChoice = "scissors";
 		break; // use break to end switch
 }
-console.log(enemyChoice);
+
+// Game logic
+let result = "";
+
+//switch always set to true
+switch (true) {
+	// check for draw conditions
+	case (lowerCaseInput === "rock" && enemyChoice === "rock") ||
+		(lowerCaseInput === "paper" && enemyChoice === "paper") ||
+		(lowerCaseInput === "scissors" && enemyChoice == "scissors"):
+		result = "draw";
+		break;
+	// check for loose conditions
+	case (lowerCaseInput === "rock" && enemyChoice === "paper") ||
+		(lowerCaseInput === "paper" && enemyChoice === "scissors") ||
+		(lowerCaseInput === "scissors" && enemyChoice === "rock"):
+		result = "loose";
+		break;
+	// check for win conditions
+	case (lowerCaseInput === "rock" && enemyChoice === "scissors") ||
+		(lowerCaseInput === "paper" && enemyChoice === "rock") ||
+		(lowerCaseInput === "scissors" && enemyChoice === "paper"):
+		result = "WON!";
+		break;
+	// catch wrong user input not valid in this game
+	case lowerCaseInput !== "rock" ||
+		lowerCaseInput !== "paper" ||
+		lowerCaseInput !== "scissors":
+		console.error("Wrong input, please try again!");
+		return;
+}
+// output to console
+console.log(
+	`You choose: ${lowerCaseInput}. Your enemy choose: ${enemyChoice}. You ${result}`
+);
