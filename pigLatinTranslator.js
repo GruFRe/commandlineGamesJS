4913065d9c25ebb29111c3720bbb4ddc8e64eaec
grufre349 <grufre349@gmail.com>
2025-07-27 18:28:12 +0200

    Pig Latin translator working

--- pigLatinTranslator.js
+++ pigLatinTranslator.js
@@ -25,29 +25,31 @@ const consonants = [
 	"n",
 	"m",
 ];
-retString = "";
+let retString = "";
+let wordToAdd = "";
 args.forEach((element) => {
 	// console.log(element);
 	// check starting chars for each element and translate element accordingly to rules
 	if (vowels.includes(element[0].toLowerCase())) {
-		// // check for starting with vocals
-		// // add "way" to the end of the string
-		// retString = retString + element + "way";
-		// // console.log(element);
+		// check for starting with vocals
+		// add "way" to the end of the string
+		wordToAdd = element + "way";
+		retString = retString + wordToAdd.toLowerCase() + " ";
+		// console.log(element);
 	} else if (
 		consonants.includes(element[0].toLowerCase()) &&
-		consonants.includes[element[1].toLowerCase()]
+		consonants.includes(element[1].toLowerCase())
 	) {
 		// check for starting with two consonants
 		// Cut first two consonatns and put them at the end of the word
 		// also add "ay" to the end
-
+		// console.log(element);
+		// console.log(element.slice(0, 2));
+		// console.log(element.slice(2, element.length));
 		// create new variable and assign substrings and additional chars
-		// let wordToAdd =
-		// 	element.slice(2, -1) + element.slice(0, element.length + 1) + "ay";
-		// retString = retString + " " + wordToAdd;
-		console.log(element.slice(0, 2));
-		console.log(element.slice(2, element.length));
+		wordToAdd =
+			element.slice(2, element.length) + element.slice(0, 2) + "ay";
+		retString = retString + wordToAdd.toLowerCase() + " ";
 	} else if (
 		consonants.includes(element[0].toLowerCase()) &&
 		vowels.includes(element[1].toLowerCase())
@@ -55,9 +57,16 @@ args.forEach((element) => {
 		// check for starting with a consonant and a vowel
 		// cut first item and put it at the end of the string
 		// add "ay" to the end
+
 		// console.log(element);
+		// console.log(element.slice(1, element.length));
+		// console.log(element.slice(0, 1));
+		wordToAdd =
+			element.slice(1, element.length) + element.slice(0, 1) + "ay";
+		retString = retString + wordToAdd.toLowerCase() + " ";
 	}
 });
+retString = retString.trim();
 console.log(retString);
 // const test = "Hello";
 // console.log(test.slice(2, test.length));
