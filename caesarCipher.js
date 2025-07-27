494587a8cb577067701fbae866e9d81818eba83c
grufre349 <grufre349@gmail.com>
2025-07-27 19:28:40 +0200

    added small helper function to shift char by given integer, refactored check#2 for shiftBy

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,7 +1,8 @@
 // processing user input and get rid of first two inbuilt ones
 const args = process.argv.slice(2);
 
-// console.log("a".charCodeAt(0));
+// console.log("a".charCodeAt(0)); // convert to ASCII from CHAR
+// test = String.fromCharCode(97); // convert from ASCII to CHAR
 
 if (args.length !== 2) {
 	console.error(
@@ -13,12 +14,21 @@ if (args.length !== 2) {
 inputString = args[0];
 // check for type and throw error if neccesary
 try {
+	// console.log(typeof args[1]);
 	shiftBy = parseInt(args[1]);
+	// console.log(typeof shiftBy);
 } catch {
-	console.error("Your secong argument is not a Number, please try again!");
+	console.error("Your secong argument is not a Number:1, please try again!");
 }
-// second error check wich works ---> WHY?
-if (shiftBy !== typeof Number) {
+if (shiftBy === NaN) {
 	console.error("Your secong argument is not a Number:2, please try again!");
 	return;
 }
+// console.log(inputString);
+// get value of each used char
+// shift it by the given value in helper function?
+
+// Helper function to shift char by int based on ASCII
+const shiftAscii = (charIn, shiftBy) => {
+	return String.fromCharCode(charIn.charCodeAt(0) + shiftBy);
+};
