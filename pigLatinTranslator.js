a304f6bac13d99f96e1b9c075c7a67fc67cb3386
grufre349 <grufre349@gmail.com>
2025-07-27 17:47:10 +0200

    first condition working in pigLatinTranslator, commented out for troubleshooting in second condition

--- pigLatinTranslator.js
+++ pigLatinTranslator.js
@@ -25,21 +25,39 @@ const consonants = [
 	"n",
 	"m",
 ];
-
+retString = "";
 args.forEach((element) => {
 	// console.log(element);
 	// check starting chars for each element and translate element accordingly to rules
-	if (vowels.includes(element[0])) {
-		// check for starting with vocals
-		console.log(element);
+	if (vowels.includes(element[0].toLowerCase())) {
+		// // check for starting with vocals
+		// // add "way" to the end of the string
+		// retString = retString + element + "way";
+		// // console.log(element);
 	} else if (
-		consonants.includes(element[0]) &&
-		consonants.includes[element[1]]
+		consonants.includes(element[0].toLowerCase()) &&
+		consonants.includes[element[1].toLowerCase()]
 	) {
 		// check for starting with two consonants
-		console.log();
-	} else if (consonants.includes(element[0]) && vowels.includes(element[1])) {
+		// Cut first two consonatns and put them at the end of the word
+		// also add "ay" to the end
+
+		// create new variable and assign substrings and additional chars
+		// let wordToAdd =
+		// 	element.slice(2, -1) + element.slice(0, element.length + 1) + "ay";
+		// retString = retString + " " + wordToAdd;
+		console.log(element.slice(0, 2));
+		console.log(element.slice(2, element.length));
+	} else if (
+		consonants.includes(element[0].toLowerCase()) &&
+		vowels.includes(element[1].toLowerCase())
+	) {
 		// check for starting with a consonant and a vowel
-		console.log(element);
+		// cut first item and put it at the end of the string
+		// add "ay" to the end
+		// console.log(element);
 	}
 });
+console.log(retString);
+// const test = "Hello";
+// console.log(test.slice(2, test.length));
